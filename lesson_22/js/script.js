42ac1e889864861b9088a027725723a56f3fe779
DenisenkoVlad <98474602+DenisenkoVlad@users.noreply.github.com>
2023-01-12 10:19:55 +0200

    Add files via upload

---
+++ lesson_22/js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+console.log('Другий урок (синтаксис)');
+console.log('Я вчу JS');
+
+console.log('Третій урок (var/let/const)');
+let userEyesColor;
+let user;
+let userName = 'Вася';
+user = userName;
+console.log(user);
